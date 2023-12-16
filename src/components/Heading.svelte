0cedb37744fe1fa6b946e406e0d1fbb7c4411aa0
bubbleboy04 <96508239+bubbleboy04@users.noreply.github.com>
2023-12-15 21:01:07 -0600

    updated fonts

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -10,7 +10,7 @@
 <div class="fullScreen">
     <div>
         <p class="nameText fadeTextAnimation" style="--delay: 1s;">Matthew Low</p>
-        <p class="nameText fadeTextAnimation" style="font-size: 3em; --delay: 3s;"> software developer</p>
+        <p class="nameText fadeTextAnimation" style="font-size: 3em; --delay: 3s;">Software Developer</p>
     </div>
 
     <div style="width: fit-content">
